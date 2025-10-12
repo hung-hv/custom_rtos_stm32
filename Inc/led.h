bc42d5c6776ba466c76103d28ef69514fbe7d812
HAU5HC <hung.haviet@vn.bosch.com>
2025-10-13 00:57:05 +0700

    update system const and led.c file

---
+++ Inc/led.h
@@ -0,0 +1,7 @@
+#ifndef __LED_H__
+#define __LED_H__
+
+
+#include "stm32f4xx.h"
+
+#endif
---
+++ Src/led.c
@@ -0,0 +1,13 @@
+#include "led.h"
+
+void led_init() {
+
+}
+
+void led_on(void) {
+
+}
+
+void led_off(void) {
+
+}
