e35a99ba31392e9ebe524403a36f71129ce81f7a
HAU5HC <hung.haviet@vn.bosch.com>
2025-11-17 19:25:11 +0700

    UART init

--- Src/uart.c
+++ Src/uart.c
@@ -1,7 +1,17 @@
 #include "uart.h"
+
 #include "stm32f4xx.h"
 
 #define GPIOAEN 	(1U << 0)
+#define USART2EN 	(1U << 17)
+
+#define SYS_FREQ	16000000
+#define APB1_CLK	SYS_FREQ
+#define UART_BAUDRATE	115200
+#define UART_CR1_TE		(1U << 3)
+#define UART_CR1_UE		(1U << 13)
+
+#define SR_TXE			(1U << 7)
 
 void uart_tx_init(void) {
 	/* enable UART2 - PA2-Tx, PA3-Rx*/
@@ -12,10 +22,25 @@ void uart_tx_init(void) {
 	GPIOA->MODER |=  (1U << 5);
 	GPIOA->MODER &= ~(1U << 4);
 
-	/* set alternate function pin to UART TX*/
+	/* set alternate function pin to UART TX PF7: 0111*/
 	GPIOA->AFR[0]  &= ~(1U << 11);
 	GPIOA->AFR[0]  |=  (1U << 10);
 	GPIOA->AFR[0]  |=  (1U << 9);
 	GPIOA->AFR[0]  |=  (1U << 8);
+
+	/* enable clock for UART2 */
+	RCC->APB1ENR != USART2EN;
+
+	/* Config baudrate*/
+
+	/* Config transfer direction: set TE in CR1 */
+	USART2->CR1 |= UART_CR1_TE;
+	/* enable UART module by set bit UE in CR1*/
+	USART2->CR1 |= UART_CR1_UE;
+
+}
+
+void uart_write(void) {
+	/* check transmit status */
 
 }
